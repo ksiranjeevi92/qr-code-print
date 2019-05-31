437970c0e7f2bfd5ef422d2be1c84405e3bdd1c7
ksiranjeevi92 <rksj666@gmail.com>
2019-05-31 13:17:44 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   printData = []
 
   constructor(private dataSource: DataSource) {
-    this.printData = this.dataSource.getbarcodeData();
+    this.printData = this.dataSource.getQrCodeData();
     //   for(let i = 0; i<= 1000; i++) {
     //   this.printData.push(this.data1);
     // }
@@ -63,13 +63,6 @@ export class AppComponent {
         margin: 0;
       }
       }
-      .container {
-        padding: 0;
-        height: 50mm;
-        width: 100mm;
-        margin: 0;
-        border: 1px solid black;
-      }
       .page-break {
         page-break-after:always;
       }
@@ -83,30 +76,56 @@ export class AppComponent {
 <body>
 <div style="position: relative;display: flex;width: 100mm;maxwidth: 100mm;flex-wrap: wrap">
 ${this.printData.map((item, i) => `
-<div id="'pageNumber'${pageNumber}" class="page-break" style="height: 25mm;width: 50mm;position: relative;">
-  <div style="margin-right: 1mm;margin-left: 1mm;margin-top: 1mm;margin-bottom: 1mm;height: 23mm;width: 48mm">
-      <div style="display: table;height: 23mm;width: 181px;border-spacing: 4px;">
-        <div style="display: table-row;width: 176px;max-width: 176px;height: 23mm;max-height: 23mm">
+<div id="barcode-label" class="page-break" style="height: 24mm;width: 50mm;position: relative;">
 
-          <div style="display: table-cell;max-width: 100px;width: 76px;background: red;height: 23mm;max-height: 23mm">
-            <img src="${"https://chart.googleapis.com/chart?cht=qr&chl=" + item.boxCode + "&chs=73x73&chld=L|0"}">
+
+  <div style="margin-right: 1mm;margin-bottom: 1mm; margin-top: 1mm;marging-left: 4mm;height: 22mm;width: 45mm">
+
+
+      <div id="table1" style="display: table;height: 22mm;width: 170px;border-spacing: 4px;background: yellow">
+        <div style="display: table-row;width: 166px;max-width: 166px;height: 22mm;max-height: 22mm">
+
+          <div style="display: table-cell;max-width: 60px;width: 60px;background: red;text-align: center;vertical-align: middle">
+            <img src="${"https://chart.googleapis.com/chart?cht=qr&chl=" + item.boxCode + "&chs=53x62&chld=L|0"}">
+
+
+            <div style="display: table-cell;text-align: center; width: 60px;max-width: 60px;overflow: hidden;">
+            <span style="font-family: Impact;font-size: 12px;">ABCDEFGH</span>
+          </div>
+
+
           </div>
          
-          <div style="display: table-cell;max-width: 100px;width: 100px;background: pink;height: 23mm;max-height: 23mm">
+          <div style="display: table-cell;max-width: 106px;width: 106px;background: pink;">            
            ${item.flowers.map((item2, i) => `
-      
-                <span style="display: table-column">${item2.baseQuantity}</span>
-                <div style="display: table-column">${item2.description}</div>
-                <div style="display: table-column">${item2.color}</div>
-                <div style="display: table-column">${item2.size}</div>
-      
+           <div style="display: table-row;font-family: Impact;font-size:10px;width: 106px;max-width: 106px;overflow-y: hidden">
+                <span>${item2.baseQuantity}</span>
+                <div>${item2.description}</div>
+                <div>${item2.color}</div>
+                <div>${item2.size}</div>
+           </div>
             `.trim()).join('')}
+            <div style="display: table">
+               <div style="display: table-row">
+               <div style="display: table-column">one</div>
+               <div style="display: table-column">two</div>
+             </div>
+            </div>
+             <div style=";background: purple;text-align: center;width: 106px;max-width: 106px;overflow: hidden;">
+               <span style="font-family: Calibri;font-size: 13px;font-weight: bold">ULGKS@99</span>
+            </div>
+            
           </div>
-        
+
+
         </div>
-        <div></div>
       </div>
+
+  
+
   </div>
+
+
 </div>
   `.trim()).join('')}
 </div>
